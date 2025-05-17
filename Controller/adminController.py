a62413702e66990e3f4c250234f3890d1682eaae
williamfi76 <wfrederickiv76@gmail.com>
2025-05-17 13:23:47 +0700

    Ini Lupa

---
+++ Controller/adminController.py
@@ -0,0 +1,18 @@
+def addNewShowing():
+    pass
+
+def getAllTransactions():
+    pass
+
+def getAllMoviesStatistics():
+    pass
+
+def getAllFoodBeveragesStatistics():
+    pass
+
+def addNewMovie():
+    pass
+
+def addNewFoodBeverage():
+    pass
+
